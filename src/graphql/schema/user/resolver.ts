487d502c71fdfa24d3994dbd6a5c1b54ae536628
Janghan Park <janghan0412@gmail.com>
2022-11-03 23:39:48 +1100

    Handle sign up with graphql and sign in with next-auth

--- src/graphql/schema/user/resolver.ts
+++ src/graphql/schema/user/resolver.ts
@@ -1,12 +1,44 @@
-import { Resolver, Query, Arg, Ctx } from "type-graphql";
+import { Resolver, Query, Arg, Mutation } from "type-graphql";
+import { GraphQLError } from "graphql";
+import bcrypt from "bcryptjs";
 
 import { User } from "./type";
-import type { MyContext } from "../../context";
+import { prisma } from "../../../lib/prisma";
+import { User as UserPrisma } from "@prisma/client";
 
 @Resolver(User)
 export class UserResolver {
   @Query(() => [User])
-  async getUsers (@Ctx() ctx: MyContext) {
-    return await ctx.prisma.user.findMany();
+  async getUsers() {
+    return await prisma.user.findMany();
+  }
+
+  @Mutation(() => User)
+  async register(
+    @Arg("email") email: string,
+    @Arg("name") name: string,
+    @Arg("password") password: string
+  ): Promise<UserPrisma> {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (user) {
+      throw new GraphQLError("The email is already in use");
+    }
+
+    const hashedPasswrod = await bcrypt.hash(password, 12);
+
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPasswrod,
+      },
+    });
+
+    return newUser;
   }
 }
